d78ba20785861e7592d0d52fff52cb534d872598
Cienna Nguyen <4326@holbertonschool.com>
2022-04-05 14:30:08 +1000

    Merge strcmp and strlen into stringcontrol

--- stringcontrol.c
+++ stringcontrol.c
@@ -4,7 +4,6 @@
 /**
   * _strlen - returns the length of a string.
   * @s: string to check
-  *
   * Return: return the length.
   */
 int _strlen(char *s)
@@ -12,9 +11,7 @@ int _strlen(char *s)
 	int count = 0;
 
 	while (*(s + count) != '\0')
-	{
 		count++;
-	}
 
 	return (count);
 }
@@ -31,19 +28,34 @@ char *_strdup(char *str)
 	char *dupStr;
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
 	length = _strlen(str);
 	dupStr = malloc((length + 1) * sizeof(char));
 	if (dupStr == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < length; i++)
-	{
 		dupStr[i] = str[i];
-	}
 
 	return (dupStr);
+}
+
+/**
+ * _strcmp - compares two strings.
+ *
+ * @s1:string 1
+ * @s2:string 2
+ * Return: copied string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		else
+			i++;
+	}
+	return (0);
 }
