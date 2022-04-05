0c4b4a0455d072076129c6f97a2b711473a47954
jql11 <4325@holbertonschool.com>
2022-04-04 23:11:15 -0700

    update strcat

--- main.h
+++ main.h
@@ -17,9 +17,8 @@ int _env(void);
 int _strlen(char *s);/* returns the length of a string.*/
 char *_strdup(char *str);/* return a pointer to the new string */
 int _strcmp(char *s1, char *s2);/* compare 2 strings */
-
+char *_strcat(char *dest, char *src)/*concatenates two strings*/
 /* get the whole path including the path and filename */
 char *_getpath(char *envirname, char *command);
 
-int exit();
 #endif
