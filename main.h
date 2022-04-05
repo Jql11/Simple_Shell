9397dfec21decd5832d794fe7dae9f5e095c7632
Cienna Nguyen <4326@holbertonschool.com>
2022-04-05 14:35:45 +1000

    Reorganise functions

--- main.h
+++ main.h
@@ -13,14 +13,10 @@ int getexecve(char *command[], char *argv[], char *envp[]);
 /* built-in function */
 int _env(void);
 
-/* self-create function */
-int _strlen(char *s);
-
-/* returns the length of a string.*/
-int _strlen(char *s);
-
-/* return a pointer to the new string */
-char *_strdup(char *str);
+/* string manipulation */
+int _strlen(char *s);/* returns the length of a string.*/
+char *_strdup(char *str);/* return a pointer to the new string */
+int _strcmp(char *s1, char *s2);/* compare 2 strings */
 
 /* get the whole path including the path and filename */
 char *_getpath(char *envirname, char *command);
