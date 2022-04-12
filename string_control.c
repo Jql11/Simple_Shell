459250abe81db81396e56ddc052a0b08a6166090
yuan-fang-228 <fy0228@gmail.com>
2022-04-11 19:41:05 -0700

    changed strcmp

--- string_control.c
+++ string_control.c
@@ -34,16 +34,18 @@ char *_strdup(char *str)
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
+	while ((*s1 != '\0') || (*s2 != '\0'))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
 	}
 	return (0);
 }
+
 
 /**
   * _strcat - concatenate two strings
