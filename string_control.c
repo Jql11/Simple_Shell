f6345fb29f3bd520bf61f5b3d400a392e15be85d
yuan-fang-228 <fy0228@gmail.com>
2022-04-12 18:15:56 +1000

    rolled back strcmp

--- string_control.c
+++ string_control.c
@@ -34,16 +34,20 @@ char *_strdup(char *str)
  */
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 != '\0') || (*s2 != '\0'))
+	int flag=0;
+
+	while(*s1 != '\0' && *s2 != '\0')
 	{
-		if (*s1 != *s2)
-		{
-			return (*s1 - *s2);
-		}
+		if(*s1 != *s2)
+			flag = 1;
 		s1++;
 		s2++;
 	}
-	return (0);
+
+	if (flag == 0)
+		return 0;
+	else
+		return 1;
 }
 
 
